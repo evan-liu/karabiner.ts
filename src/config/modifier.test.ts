bf88003c24063ea28a0823135cf7c5d45acecd7d
Evan Liu <evancoding@gmail.com>
2023-04-22 19:04:04 +1200

    ✨ Add ModifierParam and parseModifierParam()

---
+++ src/config/modifier.test.ts
@@ -0,0 +1,37 @@
+import { expect, test } from 'vitest'
+import { parseFromModifierParams, parseModifierParam } from './modifier.ts'
+
+test('parseModifierParam()', () => {
+  expect(parseModifierParam()).toBeUndefined()
+  // Modifier
+  expect(parseModifierParam('left_command')).toEqual(['left_command'])
+  // ModifierKeyAlias
+  expect(parseModifierParam('⌥')).toEqual(['option'])
+  // Array<Modifier | ModifierKeyAlias>
+  expect(parseModifierParam(['right_command', '⌥'])).toEqual([
+    'right_command',
+    'option',
+  ])
+  // MultiModifierAlias
+  expect(parseModifierParam('⌃⇧')).toEqual(['control', 'shift'])
+})
+
+test('parseFromModifierParams()', () => {
+  expect(parseFromModifierParams()).toBeUndefined()
+  expect(parseFromModifierParams('⌘')).toEqual({ mandatory: ['command'] })
+  expect(parseFromModifierParams({ left: '⌥' })).toEqual({
+    mandatory: ['left_option'],
+  })
+  expect(parseFromModifierParams({ left: '⌥', right: '⌃⇧' })).toEqual({
+    mandatory: ['left_option', 'right_control', 'right_shift'],
+  })
+  expect(
+    parseFromModifierParams('left_option', ['right_command', 'right_shift']),
+  ).toEqual({
+    mandatory: ['left_option'],
+    optional: ['right_command', 'right_shift'],
+  })
+  expect(parseFromModifierParams(undefined, { right: '⌥' })).toEqual({
+    optional: ['right_option'],
+  })
+})
---
+++ src/config/modifier.ts
@@ -0,0 +1,85 @@
+import { FromEvent, Modifier } from '../karabiner/karabiner-config'
+import { ModifierKeyAlias, modifierKeyAliases } from '../utils/key-alias'
+import {
+  MultiModifierAlias,
+  multiModifierAliases,
+} from '../utils/multi-modifier'
+
+export type ModifierParam =
+  | Modifier
+  | ModifierKeyAlias
+  | Array<Modifier | ModifierKeyAlias>
+  | MultiModifierAlias
+
+export function parseModifierParam(
+  src?: ModifierParam,
+): Modifier[] | undefined {
+  if (!src) return undefined
+
+  if (typeof src !== 'string') return src.map(getModifierWithAlias)
+
+  if (src in modifierKeyAliases)
+    return [modifierKeyAliases[src as ModifierKeyAlias]]
+
+  if (src in multiModifierAliases) {
+    return multiModifierAliases[src as MultiModifierAlias]
+  }
+
+  return [src as Modifier]
+}
+
+export type FromModifierParam =
+  | ModifierParam
+  | { left?: ModifierParam; right?: ModifierParam }
+  | 'any'
+
+export function parseFromModifierParams(
+  mandatoryParam?: FromModifierParam,
+  optionalParam?: FromModifierParam,
+): FromEvent['modifiers'] {
+  if (!mandatoryParam && !optionalParam) return undefined
+  return {
+    mandatory: parseFromModifiers(mandatoryParam),
+    optional: parseFromModifiers(optionalParam),
+  }
+}
+
+function parseFromModifiers(
+  param?: FromModifierParam,
+): Modifier[] | ['any'] | undefined {
+  if (!param) return undefined
+  if (param === 'any') return ['any']
+
+  if (typeof param === 'string' || Array.isArray(param)) {
+    return parseModifierParam(param)
+  }
+
+  const left = parseSideModifier('left', param.left)
+  const right = parseSideModifier('right', param.right)
+  if (!left?.length && !right?.length) return undefined
+
+  return [...(left || []), ...(right || [])]
+}
+
+const sidedModifiers = new Set<string>([
+  'command',
+  'option',
+  'control',
+  'shift',
+] satisfies Modifier[])
+
+function parseSideModifier(
+  side: 'left' | 'right',
+  src?: ModifierParam,
+): Modifier[] | undefined {
+  if (!src) return undefined
+  return parseModifierParam(src)?.map((v) =>
+    sidedModifiers.has(v) ? (`${side}_${v}` as Modifier) : v,
+  )
+}
+
+function getModifierWithAlias(src: Modifier | ModifierKeyAlias): Modifier {
+  if (src in modifierKeyAliases)
+    return modifierKeyAliases[src as ModifierKeyAlias]
+  return src as Modifier
+}
