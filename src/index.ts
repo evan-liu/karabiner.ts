32bf951e2af85083a8ebfad8f09c9c9072dd353b
Evan Liu <evancoding@gmail.com>
2023-04-22 22:19:44 +1200

    🚀 Add index.ts and update package.json for npm publishing

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export * from './karabiner/key-code.ts'
+export * from './karabiner/karabiner-config.ts'
+
+export * from './utils/key-alias.ts'
+export * from './utils/multi-modifier.ts'
+
+export * from './config/condition.ts'
+export * from './config/from.ts'
+export * from './config/layer.ts'
+export * from './config/manipulator.ts'
+export * from './config/modifier.ts'
+export * from './config/rule.ts'
+export * from './config/to.ts'
+
+export * from './output.ts'
