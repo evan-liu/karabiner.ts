dcf4da593a974ddc51a7004737139e4e59708166
Evan Liu <evancoding@gmail.com>
2023-04-24 15:21:57 +1200

    ✨ Add mapSimultaneous()

--- src/config/from.test.ts
+++ src/config/from.test.ts
@@ -1,9 +1,28 @@
 import { expect, test } from 'vitest'
-import { map } from './from'
+import { map, mapSimultaneous } from './from'
 
 test('map()', () => {
   expect(map('/', '⌘⌥', '⌃').build().from).toEqual({
     key_code: 'slash',
     modifiers: { mandatory: ['command', 'option'], optional: ['control'] },
+  })
+})
+
+test('mapSimultaneous()', () => {
+  const { from, parameters } = mapSimultaneous(
+    ['a', '←', 2],
+    { key_up_order: 'strict' },
+    123,
+  ).build()
+  expect(from).toEqual({
+    simultaneous: [
+      { key_code: 'a' },
+      { key_code: 'left_arrow' },
+      { key_code: '2' },
+    ],
+    simultaneous_options: { key_up_order: 'strict' },
+  })
+  expect(parameters).toEqual({
+    'basic.simultaneous_threshold_milliseconds': 123,
   })
 })
--- src/config/from.ts
+++ src/config/from.ts
@@ -2,6 +2,7 @@ import { FromKeyCode } from '../karabiner/key-code'
 import { getKeyWithAlias, KeyAlias, NumberKeyValue } from '../utils/key-alias'
 import { FromModifierParam, parseFromModifierParams } from './modifier'
 import { ManipulatorBuilder } from './manipulator'
+import { SimultaneousOptions } from '../karabiner/karabiner-config'
 
 export type FromKeyParam = FromKeyCode | KeyAlias | NumberKeyValue
 
@@ -14,4 +15,20 @@ export function map(
     key_code: getKeyWithAlias(key) as FromKeyCode,
     modifiers: parseFromModifierParams(mandatoryModifiers, optionalModifiers),
   })
+}
+
+export function mapSimultaneous(
+  keys: FromKeyParam[],
+  options?: SimultaneousOptions,
+  threshold?: number,
+) {
+  const manipulatorBuilder = new ManipulatorBuilder({
+    simultaneous: keys.map((v) => ({ key_code: getKeyWithAlias(v) })),
+    simultaneous_options: options,
+  })
+  return threshold
+    ? manipulatorBuilder.parameters({
+        'basic.simultaneous_threshold_milliseconds': threshold,
+      })
+    : manipulatorBuilder
 }
