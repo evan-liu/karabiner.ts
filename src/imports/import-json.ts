546fe752f5f1937ba83d7daf63a19e02a0120c6a
Evan Liu <evancoding@gmail.com>
2023-12-26 18:41:58 +1300

    ✨ Add importJson() and importProfile()

---
+++ src/imports/import-json.ts
@@ -0,0 +1,26 @@
+import { RuleBuilder } from '../config/rule.ts'
+import { Manipulator, Rule } from '../karabiner/karabiner-config.ts'
+import { writeContext } from '../output.ts'
+
+/**
+ * Imports from a JSON file.
+ *
+ * @param filePath - The path to the JSON file to import.
+ */
+export function importJson(filePath: string): RuleBuilder {
+  return {
+    build(): Rule {
+      const json = writeContext.readJson(filePath)
+      if (!Array.isArray(json?.rules))
+        throw new Error(`Cannot file rules in ${filePath}`)
+
+      return {
+        description: `Imported from ${filePath}`,
+        manipulators: (json.rules as Rule[]).reduce(
+          (r, v) => r.concat(v.manipulators),
+          [] as Manipulator[],
+        ),
+      }
+    },
+  }
+}
---
+++ src/imports/import-profile.ts
@@ -0,0 +1,32 @@
+import { RuleBuilder } from '../config/rule.ts'
+import {
+  KarabinerConfig,
+  Manipulator,
+  Rule,
+} from '../karabiner/karabiner-config.ts'
+import { writeContext } from '../output.ts'
+
+/**
+ * Import from karabiner config profile
+ *
+ * @param name        The profile name in the config JSON
+ * @param configFile  The config JSON file path. Default ~/.config/karabiner/karabiner.json
+ */
+export function importProfile(name: string, configFile?: string): RuleBuilder {
+  return {
+    build(): Rule {
+      const config: KarabinerConfig =
+        writeContext.readKarabinerConfig(configFile)
+      const profile = config.profiles.find((v) => v.name === name)
+      if (!profile) throw new Error(`Profile ${name} not found`)
+
+      return {
+        description: `Imported from profile ${name}`,
+        manipulators: profile.complex_modifications.rules.reduce(
+          (r, v) => r.concat(v.manipulators),
+          [] as Manipulator[],
+        ),
+      }
+    },
+  }
+}
