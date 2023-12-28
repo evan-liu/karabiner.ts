0cb7fae0ac84a83618065d5736a6afc03d4a95c5
Evan Liu <evancoding@gmail.com>
2023-12-29 10:56:56 +1300

    ✨ [editor] Start the editor project

---
+++ editor/rules.js
@@ -0,0 +1,20 @@
+// prettier-ignore
+import {
+// rule and layers
+rule, layer, simlayer, hyperLayer, modifierLayer, duoLayer,
+// from / map()
+map, mapConsumerKey, mapPointingButton, mapSimultaneous, mapDoubleTap,
+// to
+toKey, toConsumerKey, toPointingButton, toHyper, to$, toApp, toPaste,
+// conditions
+ifApp, ifDevice, ifVar,
+// utils
+withCondition, withMapper
+} from 'karabiner.ts';
+
+export const rules = () => [
+  rule('Playground').manipulators([
+    map('⇪').toHyper().toIfAlone('⎋'),
+    { esc: toKey('caps_lock') },
+  ]),
+]
