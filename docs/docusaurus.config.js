094a608f72d75db4faf97c11eaab7c71403e57bf
Evan Liu <evancoding@gmail.com>
2024-01-09 08:15:54 +1300

    📝 [docs] Add search to docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -66,6 +66,11 @@ const config = {
         darkTheme: darkCodeTheme,
         additionalLanguages: ['json'],
       },
+      algolia: {
+        appId: '8FCBVUX23W',
+        apiKey: '577f1af3c7191bb53e86200751990c4e',
+        indexName: 'karabiner-ts',
+      },
     }),
 }
 
