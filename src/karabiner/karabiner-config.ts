17dafecac1852656490288d1329cc12379eee88f
Evan Liu <evanliu.dev@gmail.com>
2025-07-27 09:33:55 +1200

    ✨ Add writeToGlobal() output method

--- src/karabiner/karabiner-config.ts
+++ src/karabiner/karabiner-config.ts
@@ -340,4 +340,11 @@ export type KarabinerProfile = {
   complex_modifications: ComplexModifications
 }
 
-export type KarabinerConfig = { profiles: [KarabinerProfile] }
+export type KarabinerConfig = {
+  global?: {
+    check_for_updates_on_startup?: boolean
+    show_in_menu_bar?: boolean
+    show_profile_name_in_menu_bar?: boolean
+  }
+  profiles: [KarabinerProfile]
+}
